9b1018ea38bcf544583d1d875dfda84f6a004a53
Eliseu Oliveira <eliseu.uepb@gmail.com>
2021-06-22 17:28:26 -0300

    finalizando aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from "express"
+import "reflect-metadata";
+import express from "express";
+import "./database"
+import { router } from "./routes"
+
 
 const app = express();
-const PATH = "/valoriza"
+app.use(express.json())
+app.use(router)
 
-app.get(PATH + "/test", (req, res) => {
-    return res.send("Hello NLW Together")
-})
-app.post(PATH + "/test-post", (req, res) => {
-    return res.send("Hello test post")
-})
+app.listen(3003, () => console.log('Server is running now'));
 
-app.listen(3003, () => console.log("Server is running"))
+
